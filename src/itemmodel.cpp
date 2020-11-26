cscarney/syndic

commit 640e31491889d7db9230ff02430bef88c7f9469e

new storage backend

src/itemmodel.cpp
@@ -32,10 +32,10 @@ QHash<int, QByteArray> ItemModel::roleNames() const
     };
 }
 
-void ItemModel::addItem(FeedSource::Item const &item)
+void ItemModel::addItem(StoredItem const &item)
 {
     if (((m_feedFilter==std::nullopt) || ((*m_feedFilter) == item.feedId))
-         && (!m_unreadFilter || item.isUnread))
+         && (!m_unreadFilter || !item.status.isRead))
     {
         beginInsertRows(QModelIndex(), m_items.size(), m_items.size());
         m_items.append(item);
@@ -43,11 +43,17 @@ void ItemModel::addItem(FeedSource::Item const &item)
     }
 }
 
-void ItemModel::updateItem(const FeedSource::Item &item)
+void ItemModel::updateItem(StoredItem const &item)
 {
     for(auto i=0; i<m_items.size(); i++) {
         if (m_items[i].id == item.id) {
-            m_items[i] = item;
+
+            // changing the date interferes with the selection
+            // when the list is sorted by date, so don't do that
+            auto newItem = item;
+            newItem.headers.date = m_items[i].headers.date;
+
+            m_items[i] = newItem;
             auto idx = index(i);
             dataChanged(idx, idx);
             break;
@@ -74,25 +80,25 @@ QVariant ItemModel::data(const QModelIndex &index, int role) const
             return m_items[indexRow].id;
 
         case Roles::Headline:
-            return m_items[indexRow].headline;
+            return m_items[indexRow].headers.headline;
 
         case Roles::Author:
-            return m_items[indexRow].author;
+            return m_items[indexRow].headers.author;
 
         case Roles::Date:
-            return m_items[indexRow].date;
+            return m_items[indexRow].headers.date;
 
         case Roles::Content:
             return m_items[indexRow].content;
 
         case Roles::Url:
-            return m_items[indexRow].url;
+            return m_items[indexRow].headers.url;
 
         case Roles::IsUnread:
-            return m_items[indexRow].isUnread;
+            return !m_items[indexRow].status.isRead;
 
         case Roles::IsStarred:
-            return m_items[indexRow].isStarred;
+            return m_items[indexRow].status.isStarred;
     }
 
     return QVariant();

src/qml/ArticleListPage.qml
@@ -33,7 +33,7 @@ Kirigami.ScrollablePage {
         if (articleList.currentItem) {
             var data = articleList.currentItem.data
             root.pageRow.push("qrc:/qml/ArticlePage.qml", {item: data})
-            if (data.isUnread) feedManager.setUnread(data.id, false)
+            if (data.isUnread) feedManager.setRead(data.id, true)
         }
     }
 

CMakeLists.txt
@@ -14,7 +14,7 @@ set(CMAKE_AUTORCC ON)
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
- find_package(Qt5 COMPONENTS Core Qml Widgets Quick QuickControls2 REQUIRED)
+ find_package(Qt5 COMPONENTS Core Qml Widgets Quick QuickControls2 Sql REQUIRED)
  
  find_package(KF5Declarative REQUIRED)
  find_package(KF5Crash REQUIRED)
@@ -23,6 +23,10 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
  
  set(CMAKE_CXX_FLAGS_DEBUG "-Wall -Werror")
  set(CMAKE_CXX_FLAGS_RELEASE "-O3 -flto")
+ 
+ include_directories(feedsource feedstorage src)
 
+ add_subdirectory(feedsource)
+ add_subdirectory(feedstorage)
  add_subdirectory(src)
 

feedsource/CMakeLists.txt
@@ -1,0 +1,17 @@
+set(feedsource_HEADERS
+    feedsource.h
+    ttrssfeedsource.h
+    )
+    
+set(feedsource_SRCS
+    ${feedsource_HEADERS}
+    ttrssfeedsource.cpp
+    )
+    
+add_library(feedsource ${feedsource_SRCS})
+
+target_link_libraries(feedsource
+    Qt5::Core
+    Qt5::Network
+    KF5::Syndication
+)

feedsource/feedsource.h
@@ -2,7 +2,7 @@
 #define FEEDSOURCE_H
 
 #include <QObject>
-#include <QNetworkAccessManager>
+#include <QDateTime>
 
 #include <optional>
 #include <functional>

feedsource/ttrssfeedsource.cpp
@@ -66,7 +66,6 @@ void TTRSSFeedSource::beginUpdate()
     m_fullUpdate = (m_lastId == 0);
 
     qDebug("getting new items since %lld", m_lastId);
-    getHeadlines(m_lastId);
     getFeeds();
 }
 
@@ -287,6 +286,8 @@ void TTRSSFeedSource::gotFeeds(QJsonDocument const &doc, QJsonObject const & /*r
      *       ]
      * }
      */
+    getHeadlines(m_lastId);
+
     auto content = doc["content"];
     auto entries = content.toArray();
     for (auto const &item : entries)

feedsource/ttrssfeedsource.h
@@ -1,6 +1,8 @@
 #ifndef TTRSSFEEDSOURCE_H
 #define TTRSSFEEDSOURCE_H
 
+#include <QNetworkAccessManager>
+
 #include <functional>
 #include <QSettings>
 #include "feedsource.h"

feedstorage/CMakeLists.txt
@@ -1,0 +1,23 @@
+set(feedstorage_HEADERS
+    feedheaders.h
+    feeditemheaders.h
+    feeditemstatus.h
+    storedfeed.h
+    storeditem.h
+    feedstorage.h
+    feeddatabase.h
+    sqlitefeedstorage.h
+    )
+    
+set(feedstorage_SRCS
+    ${feedstorage_HEADERS}
+    feeddatabase.cpp
+    sqlitefeedstorage.cpp
+    )
+    
+add_library(feedstorage ${feedstorage_SRCS})
+
+target_link_libraries(feedstorage
+    Qt5::Core
+    Qt5::Sql
+)

commit 6c88fd64156e9f6937dc8a6581e752fbb0b1ed27

Select first item when the article list has finished populating (quick fix for bug introduced by 640e31491889d7db9230ff02430bef88c7f9469e)

src/itemmodel.cpp
@@ -87,10 +87,11 @@ QAbstractItemModel *ItemModel::createSortedProxy()
 void ItemModel::slotQueryFinished()
 {
     auto *q = static_cast<FeedStorage::ItemQuery *>(QObject::sender());
-    auto &storedItems = q->result;
-    for (auto const &storedItem : storedItems) {
-        addItem(storedItem);
+    beginResetModel();
+    for (auto const &storedItem : q->result) {
+        m_items.append(storedItem);
     }
+    endResetModel();
 }
 
 int ItemModel::rowCount(const QModelIndex &parent) const

src/qml/ArticleListPage.qml
@@ -45,6 +45,13 @@ Kirigami.ScrollablePage {
            modelIteration;
            return feedManager.getModel(feedFilter, settings.unreadFilter);
        }
+
+       Connections {
+           target: articleList.model
+           onModelReset: {
+               if (articleList.currentIndex < 0) articleList.currentIndex = 0;
+           }
+       }
    } /* ArticleList */
 
     actions {