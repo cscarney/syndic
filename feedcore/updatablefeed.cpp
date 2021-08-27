40c1253c5b9673758de2d9b19917b7a695d34fff
Connor Carney <ccarney@gmail.com>
2021-08-27 10:33:07 -0500

    Timeout when server fails to respond

--- feedcore/updatablefeed.cpp
+++ feedcore/updatablefeed.cpp
@@ -105,7 +105,6 @@ void UpdatableFeed::UpdaterImpl::loadingComplete(Syndication::Loader *loader, co
         finish();
         return;
     case Syndication::Aborted:
-        qDebug() << "load aborted for " << m_updatableFeed->url();
         aborted();
         return;
     case Syndication::Timeout:
@@ -149,6 +148,7 @@ void DataRetriever::retrieveData(const QUrl &url)
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::UserAgentHeader, "curl/4.6");
     request.setAttribute(QNetworkRequest::RedirectPolicyAttribute, QNetworkRequest::NoLessSafeRedirectPolicy);
+    request.setTransferTimeout();
     m_reply = NetworkAccessManager::instance()->get(request);
     m_reply->setParent(this);
     QObject::connect(m_reply, &QNetworkReply::finished, this, &DataRetriever::onFinished);
@@ -163,7 +163,6 @@ void DataRetriever::abort()
 {
     m_reply->disconnect(this);
     m_reply->abort();
-    m_reply->deleteLater();
 }
 
 void DataRetriever::onFinished()
