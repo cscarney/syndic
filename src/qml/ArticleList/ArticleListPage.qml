cscarney/syndic

commit f13146d82253029471a3fcf7b2c502b26b25acb2

Remove "Clear" button
It was not obvious what the button did; now you can re-filter by reselecting the feed in the feed list.

src/qml/ArticleList/ArticleListPage.qml
@@ -21,18 +21,6 @@ AbstractArticleListPage {
             displayHint: Kirigami.DisplayHint.KeepVisible
         }
 
-        right: Kirigami.Action {
-            id: clearReadAction
-            text: qsTr("Clear")
-            iconName: "edit-clear-all"
-            visible: root.feed && root.unreadFilter
-            enabled: root.feed && root.count > root.feed.unreadCount
-            onTriggered: {
-                root.currentIndex = -1
-                root.model.removeRead()
-            }
-        }
-
         contextualActions: [
             Kirigami.Action {
                  text: qsTr("Hide Read")

src/qml/FeedList.qml
@@ -15,6 +15,7 @@ ScrollView {
     property Feed currentlySelectedFeed
     property alias currentIndex: feedList.currentIndex
     signal itemClicked
+    signal reselectedCurrentFeed
 
     ScrollBar.vertical.policy: ScrollBar.AsNeeded
     ScrollBar.horizontal.policy: ScrollBar.AlwaysOff
@@ -88,7 +89,11 @@ ScrollView {
             }
 
             onClicked: {
-                feedList.currentIndex = index;
+                if (feedList.currentIndex === index) {
+                    reselectedCurrentFeed()
+                } else {
+                    feedList.currentIndex = index;
+                }
                 itemClicked();
             }
         }/* delegate */

src/qml/main.qml
@@ -64,6 +64,8 @@ Kirigami.ApplicationWindow {
                 Layout.preferredHeight: drawer.height
                 onCurrentlySelectedFeedChanged:
                     if (currentlySelectedFeed) pushFeed(currentlySelectedFeed)
+                onReselectedCurrentFeed:
+                    if (pageStack.items[0].clearRead) pageStack.items[0].clearRead()
                 onItemClicked:
                     drawer.drawerOpen = !drawer.modal
             }

src/CMakeLists.txt
@@ -86,7 +86,6 @@ if (ANDROID)
         globe
         checkmark
         emblem-shared-symbolic
-        edit-clear-all
         )
 endif()
 

src/qml/ArticleList/AbstractArticleListPage.qml
@@ -117,6 +117,12 @@ Kirigami.ScrollablePage {
         }
     }
 
+    function clearRead() {
+        root.currentIndex = model.status === Feed.Idle ? 0 : -1
+        root.model.removeRead()
+        articleList.positionViewAtBeginning()
+    }
+
     function pushPlaceholder() {
         switch(model.status) {
         case Feed.Loading:

commit 294e2950019f0722bfe374655e314e538bc88ed8

Don't trigger "clear read" action when initially opening a feed
Fixes bug introduced by f13146d82253029471a3fcf7b2c502b26b25acb2

src/qml/ArticleList/ArticleListPage.qml
@@ -3,12 +3,14 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import QtQml 2.15
 import com.rocksandpaper.syndic 1.0
 import org.kde.kirigami 2.14 as Kirigami
 
 AbstractArticleListPage {
     id: root
     unreadFilter: globalSettings.unreadFilter
+    property bool acceptFeedListAction: false
 
     actions {
         main: Kirigami.Action {
@@ -66,4 +68,14 @@ AbstractArticleListPage {
 
         ]
     }
+
+    function feedListAction() {
+        // ignore the one that created us...
+        if (!acceptFeedListAction) return;
+        clearRead();
+    }
+
+    Component.onCompleted: Qt.callLater(function(){
+        acceptFeedListAction = true;
+    })
 }

src/qml/FeedList.qml
@@ -15,7 +15,6 @@ ScrollView {
     property Feed currentlySelectedFeed
     property alias currentIndex: feedList.currentIndex
     signal itemClicked
-    signal reselectedCurrentFeed
 
     ScrollBar.vertical.policy: ScrollBar.AsNeeded
     ScrollBar.horizontal.policy: ScrollBar.AlwaysOff
@@ -88,14 +87,7 @@ ScrollView {
                 }
             }
 
-            onClicked: {
-                if (feedList.currentIndex === index) {
-                    reselectedCurrentFeed()
-                } else {
-                    feedList.currentIndex = index;
-                }
-                itemClicked();
-            }
+            onClicked: root.itemClicked()
         }/* delegate */
 
         section.property: "category"

src/qml/main.qml
@@ -64,10 +64,12 @@ Kirigami.ApplicationWindow {
                 Layout.preferredHeight: drawer.height
                 onCurrentlySelectedFeedChanged:
                     if (currentlySelectedFeed) pushFeed(currentlySelectedFeed)
-                onReselectedCurrentFeed:
-                    if (pageStack.items[0].clearRead) pageStack.items[0].clearRead()
-                onItemClicked:
+                onItemClicked: {
+                    if (pageStack.items[0] && pageStack.items[0].feedListAction) {
+                        pageStack.items[0].feedListAction();
+                    }
                     drawer.drawerOpen = !drawer.modal
+                }
             }
 
             Kirigami.Separator {