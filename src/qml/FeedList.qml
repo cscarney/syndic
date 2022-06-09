cscarney/syndic

commit f13146d82253029471a3fcf7b2c502b26b25acb2

Remove "Clear" button
It was not obvious what the button did; now you can re-filter by reselecting the feed in the feed list.

src/qml/FeedList.qml
@@ -15,6 +15,7 @@ ScrollView {
     property Feed currentlySelectedFeed
     property alias currentIndex: feedList.currentIndex
     signal itemClicked
+    signal reselectedCurrentFeed
 
     ScrollBar.vertical.policy: ScrollBar.AsNeeded
     ScrollBar.horizontal.policy: ScrollBar.AlwaysOff
@@ -88,7 +89,11 @@ ScrollView {
             }
 
             onClicked: {
-                feedList.currentIndex = index;
+                if (feedList.currentIndex === index) {
+                    reselectedCurrentFeed()
+                } else {
+                    feedList.currentIndex = index;
+                }
                 itemClicked();
             }
         }/* delegate */

src/CMakeLists.txt
@@ -86,7 +86,6 @@ if (ANDROID)
         globe
         checkmark
         emblem-shared-symbolic
-        edit-clear-all
         )
 endif()
 

src/qml/ArticleList/AbstractArticleListPage.qml
@@ -117,6 +117,12 @@ Kirigami.ScrollablePage {
         }
     }
 
+    function clearRead() {
+        root.currentIndex = model.status === Feed.Idle ? 0 : -1
+        root.model.removeRead()
+        articleList.positionViewAtBeginning()
+    }
+
     function pushPlaceholder() {
         switch(model.status) {
         case Feed.Loading:

src/qml/ArticleList/ArticleListPage.qml
@@ -21,18 +21,6 @@ AbstractArticleListPage {
             displayHint: Kirigami.DisplayHint.KeepVisible
         }
 
-        right: Kirigami.Action {
-            id: clearReadAction
-            text: qsTr("Clear")
-            iconName: "edit-clear-all"
-            visible: root.feed && root.unreadFilter
-            enabled: root.feed && root.count > root.feed.unreadCount
-            onTriggered: {
-                root.currentIndex = -1
-                root.model.removeRead()
-            }
-        }
-
         contextualActions: [
             Kirigami.Action {
                  text: qsTr("Hide Read")

src/qml/main.qml
@@ -64,6 +64,8 @@ Kirigami.ApplicationWindow {
                 Layout.preferredHeight: drawer.height
                 onCurrentlySelectedFeedChanged:
                     if (currentlySelectedFeed) pushFeed(currentlySelectedFeed)
+                onReselectedCurrentFeed:
+                    if (pageStack.items[0].clearRead) pageStack.items[0].clearRead()
                 onItemClicked:
                     drawer.drawerOpen = !drawer.modal
             }

commit f86b16ab23fd5fd645c76e9263876c84a1bebd52

Delay changing feed until feed list has updated
Prevents blank screen (no pages in stack) when deleting the currently selected feed

Fixes regression caused by f13146d82253029471a3fcf7b2c502b26b25acb2

src/qml/FeedList.qml
@@ -34,13 +34,13 @@ ScrollView {
             sortMode: globalSettings.feedListSort
             onRowsInserted: {
                 if (feedList.currentIndex < 0) {
-                    feedList.currentIndex = -1
-                    currentlySelectedFeed = data(index(first,0), FeedListModel.FeedRole);
+                    Qt.callLater(()=>feedList.currentIndex = first);
                 }
             }
             onRowsRemoved: {
-                feedList.currentIndex = 0;
-                currentlySelectedFeed = data(index(0,0), FeedListModel.FeedRole);
+                Qt.callLater(()=>{
+                    feedList.currentIndex = 0;
+                });
             }
         }
         clip: true