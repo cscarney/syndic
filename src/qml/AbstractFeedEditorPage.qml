cscarney/syndic

commit 24fafc71f1f45ce506bbef24d5637af5d02179a3

Custom url-to-string handling that avoids producing relative URLs
fixes #9

src/qml/AbstractFeedEditorPage.qml
@@ -33,9 +33,10 @@ Kirigami.ScrollablePage {
             TextField {
                 id: urlField
                 Kirigami.FormData.label: qsTr("Feed Url:")
-                text: provisionalFeed.targetFeed ? provisionalFeed.url : ""
+                text: provisionalFeed.urlString
                 onTextChanged: previewOpen = false;
-                onTextEdited: provisionalFeed.url = text
+                onTextEdited: provisionalFeed.urlString = text
+                onEditingFinished: provisionalFeed.syncUrlString();
             }
 
             Item {

feedcore/provisionalfeed.cpp
@@ -5,6 +5,7 @@
 
 #include "provisionalfeed.h"
 #include "article.h"
+#include <QDir>
 #include <Syndication/Image>
 #include <Syndication/Item>
 #include <Syndication/Person>
@@ -15,8 +16,8 @@ class ProvisionalFeed::ArticleImpl : public Article
 public:
     explicit ArticleImpl(const Syndication::ItemPtr &item, Feed *feed, QObject *parent = nullptr);
     void requestContent() final;
-    void setRead(bool isRead) final{};
-    void setStarred(bool isStarred) final{};
+    void setRead(bool /*isRead*/) final{};
+    void setStarred(bool /*isStarred*/) final{};
 
 private:
     Syndication::ItemPtr m_item;
@@ -24,6 +25,7 @@ private:
 
 void ProvisionalFeed::onUrlChanged()
 {
+    syncUrlString();
     updater()->abort();
     m_feed = nullptr;
     emit reset();
@@ -32,6 +34,7 @@ void ProvisionalFeed::onUrlChanged()
 ProvisionalFeed::ProvisionalFeed(QObject *parent)
     : UpdatableFeed(parent)
 {
+    syncUrlString();
     QObject::connect(this, &Feed::urlChanged, this, &ProvisionalFeed::onUrlChanged);
     QObject::connect(this, &ProvisionalFeed::targetFeedChanged, this, [this] {
         updateParams(m_targetFeed);
@@ -101,4 +104,70 @@ ProvisionalFeed::ArticleImpl::ArticleImpl(const Syndication::ItemPtr &item, Feed
     setDate(QDateTime::fromTime_t(item->dateUpdated()));
     auto authors = item->authors();
     setAuthor(authors.isEmpty() ? "" : authors[0]->name());
+}
+
+const QString &ProvisionalFeed::urlString() const
+{
+    return m_urlString;
+}
+
+// This is similar to QUrl::fromUserInput, but we need slightly different behavior
+static QUrl urlFromString(const QString &string)
+{
+    QString trimmedString = string.trimmed();
+
+    if (trimmedString.isEmpty()) {
+        return QUrl();
+    }
+
+    if (QDir::isAbsolutePath(string)) {
+        return QUrl::fromLocalFile(string);
+    }
+
+    const QUrl url(trimmedString, QUrl::TolerantMode);
+    if (url.isValid() && !url.isRelative()) {
+        return url;
+    }
+
+    const QUrl httpsUrl(QLatin1String("https://") + trimmedString, QUrl::TolerantMode);
+    if (httpsUrl.isValid()) {
+        return httpsUrl;
+    }
+
+    return QUrl();
+}
+
+void ProvisionalFeed::setUrlString(const QString &newUrlString)
+{
+    if (m_urlStringStatus == PENDING) {
+        return;
+    }
+    if (m_urlString == newUrlString) {
+        return;
+    }
+    m_urlStringStatus = PENDING;
+    m_urlString = newUrlString;
+    QUrl url = urlFromString(newUrlString);
+    if (url.isValid()) {
+        setUrl(url);
+        m_urlStringStatus = VALID;
+    } else {
+        m_urlStringStatus = INVALID;
+    }
+    emit urlStringChanged();
+}
+
+void ProvisionalFeed::syncUrlString()
+{
+    if (m_urlStringStatus == PENDING) {
+        return;
+    }
+    const QUrl &url = this->url();
+    const QString urlString = url.toString();
+    if (m_urlString == urlString) {
+        return;
+    }
+    m_urlString = urlString;
+    m_urlStringStatus = url.isValid() ? VALID : INVALID;
+    emit urlStringChanged();
 }

feedcore/provisionalfeed.h
@@ -35,31 +35,63 @@ class ProvisionalFeed : public UpdatableFeed
      * This property is null by default.
      */
     Q_PROPERTY(Feed *targetFeed READ targetFeed WRITE setTargetFeed NOTIFY targetFeedChanged)
+
+    /**
+     * User input for the URL string.
+     *
+     * Setting this property will attempt to parse a URL from the string input. If urlString
+     * cannot be converted into a valid URL, the URL is not changed.
+     */
+    Q_PROPERTY(QString urlString READ urlString WRITE setUrlString NOTIFY urlStringChanged)
+
 public:
     explicit ProvisionalFeed(QObject *parent = nullptr);
+
+    enum UrlStringStatus { INVALID, VALID, PENDING };
+    Q_ENUM(UrlStringStatus)
 
     /**
      * Copy this feed's configuration into the feed specified by the targetFeed property
      */
     Q_INVOKABLE void save();
 
+    /**
+     * Reset URL string to the current feed url
+     */
+    Q_INVOKABLE void syncUrlString();
+
+    /**
+     * Whether urlString was successfully parsed into a URL.
+     */
+    Q_INVOKABLE ProvisionalFeed::UrlStringStatus urlStringStatus()
+    {
+        return m_urlStringStatus;
+    }
+
     Future<ArticleRef> *getArticles(bool unreadFilter) final;
 
     Feed *targetFeed() const;
     void setTargetFeed(Feed *targetFeed);
 
+    const QString &urlString() const;
+    void setUrlString(const QString &newUrlString);
+
 signals:
     void targetFeedChanged();
+    void urlStringChanged();
 
 private:
     Feed *m_targetFeed{nullptr};
     Syndication::FeedPtr m_feed;
+    QString m_urlString;
+    UrlStringStatus m_urlStringStatus{INVALID};
+
     class ArticleImpl;
     SharedFactory<Syndication::ItemPtr, ArticleImpl> m_articles;
     void onUrlChanged();
     void updateFromSource(const Syndication::FeedPtr &feed) final;
-    void updateSourceArticle(const Syndication::ItemPtr &) final{};
-    void expire(const QDateTime &) final{};
+    void updateSourceArticle(const Syndication::ItemPtr & /*article*/) final{};
+    void expire(const QDateTime & /*olderThan*/) final{};
 };
 }
 #endif // FEEDCORE_PROVISIONALFEED_H

test/CMakeLists.txt
@@ -15,3 +15,6 @@ add_executable(testContextValuePropagation tst_testcontextvaluepropagation.cpp)
 add_test(NAME testContextValuePropagation COMMAND testContextValuePropagation)
 target_link_libraries(testContextValuePropagation PRIVATE Qt5::Test feedcore)
 
+add_executable(testProvisionalFeed tst_provisionalFeed.cpp)
+add_test(NAME testProvisionalFeed COMMAND testProvisionalFeed)
+target_link_libraries(testProvisionalFeed PRIVATE Qt5::Test feedcore)

test/tst_provisionalFeed.cpp
@@ -1,0 +1,86 @@
+#include "provisionalfeed.h"
+#include <QCoreApplication>
+#include <QSignalSpy>
+#include <QtTest>
+using namespace FeedCore;
+
+constexpr const char *kValidUrl = "https://runningincircles.com/feed";
+constexpr const char *kUrlMissingSchema = "runningincircles.com/feed";
+constexpr const char *kInvalidUrl = "!!!";
+
+class testProvisionalFeed : public QObject
+{
+    Q_OBJECT
+    QScopedPointer<ProvisionalFeed> m_testFeed;
+    QScopedPointer<QSignalSpy> m_spyUrlChanged;
+    QScopedPointer<QSignalSpy> m_spyUrlStringChanged;
+
+private slots:
+    void initTestCase()
+    {
+        qRegisterMetaType<FeedCore::Feed *>();
+        qRegisterMetaType<FeedCore::ProvisionalFeed *>();
+    }
+
+    void init()
+    {
+        m_testFeed.reset(new ProvisionalFeed());
+        m_spyUrlChanged.reset(new QSignalSpy(m_testFeed.get(), &ProvisionalFeed::urlChanged));
+        m_spyUrlStringChanged.reset(new QSignalSpy(m_testFeed.get(), &ProvisionalFeed::urlStringChanged));
+    }
+
+    void cleanup()
+    {
+        m_testFeed.reset();
+        m_spyUrlChanged.reset();
+        m_spyUrlStringChanged.reset();
+    }
+
+    void testSetUrlStringToValidUrl()
+    {
+        m_testFeed->setUrlString(kValidUrl);
+        QVERIFY(m_testFeed->url() == QUrl(kValidUrl));
+        QVERIFY(m_spyUrlChanged->count() == 1);
+        QVERIFY(m_spyUrlStringChanged->count() == 1);
+    }
+
+    void testSetUrlStringWithoutSchema()
+    {
+        m_testFeed->setUrlString(kUrlMissingSchema);
+        QVERIFY(m_testFeed->url() == QUrl(kValidUrl));
+        QVERIFY(m_testFeed->urlString() == kUrlMissingSchema);
+        QVERIFY(m_spyUrlChanged->count() == 1);
+        QVERIFY(m_spyUrlStringChanged->count() == 1);
+    }
+
+    void testUrlStringUpdatedWhenUrlChanged()
+    {
+        m_testFeed->setUrl(QUrl(kValidUrl));
+        QVERIFY(m_testFeed->urlString() == kValidUrl);
+        QVERIFY(m_spyUrlChanged->count() == 1);
+        QVERIFY(m_spyUrlStringChanged->count() == 1);
+    }
+
+    void testUrlStringInvalidIgnored()
+    {
+        m_testFeed->setUrl(QUrl(kValidUrl));
+        m_testFeed->setUrlString(kInvalidUrl);
+        QVERIFY(m_testFeed->url() == QUrl(kValidUrl));
+        QVERIFY(m_testFeed->urlString() == kInvalidUrl);
+        QVERIFY(m_spyUrlChanged->count() == 1);
+        QVERIFY(m_spyUrlStringChanged->count() == 2);
+    }
+
+    void testUrlStringEmptyIgnored()
+    {
+        m_testFeed->setUrl(QUrl(kValidUrl));
+        m_testFeed->setUrlString("");
+        QVERIFY(m_testFeed->url() == QUrl(kValidUrl));
+        QVERIFY(m_testFeed->urlString() == "");
+        QVERIFY(m_spyUrlChanged->count() == 1);
+        QVERIFY(m_spyUrlStringChanged->count() == 2);
+    }
+};
+
+QTEST_MAIN(testProvisionalFeed)
+#include "tst_provisionalFeed.moc"

commit 881544f18c35f938e7e99d42f72b08a2bc50d4ce

Don't close preview when feed URL is changed programmatically
regression from 24fafc71f1f45ce506bbef24d5637af5d02179a3

fixes #28

src/qml/AbstractFeedEditorPage.qml
@@ -34,8 +34,10 @@ Kirigami.ScrollablePage {
                 id: urlField
                 Kirigami.FormData.label: qsTr("Feed Url:")
                 text: provisionalFeed.urlString
-                onTextChanged: previewOpen = false;
-                onTextEdited: provisionalFeed.urlString = text
+                onTextEdited: {
+                    provisionalFeed.urlString = text;
+                    previewOpen = false;
+                }
                 onEditingFinished: provisionalFeed.syncUrlString();
             }
 