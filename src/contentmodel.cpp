fab1b65f7c44a48099b3890be9a4b9e92922c016
Connor Carney <hello@connorcarney.com>
2022-09-02 18:03:58 -0500

    Build Article Content Model Asynchronously
    Rather than laying out all of the article content during the QML loading
    process, we parse the content and post a series of low-priority events
    that do the layout for each block separately.
    
    This improves responsiveness when loading large articles, although blocks
    are currently only broken at text/image boundaries, so articles that
    present a long wall of text with no images might still be sluggish.
    
    This has the possibility to cause regressions, so it shouldn't be picked
    to the release series until after 1.0.
    
    fixes #49

--- src/contentmodel.cpp
+++ src/contentmodel.cpp
@@ -5,6 +5,32 @@
 
 #include "contentmodel.h"
 
+#include <QCoreApplication>
+#include <QEvent>
+#include <memory>
+
+namespace
+{
+const auto kAddBlockEventType = static_cast<QEvent::Type>(QEvent::registerEventType());
+
+class AddBlockEvent : public QEvent
+{
+    std::unique_ptr<ContentBlock> m_contentBlock;
+
+public:
+    explicit AddBlockEvent(ContentBlock *block)
+        : QEvent(kAddBlockEventType)
+        , m_contentBlock{block}
+    {
+    }
+
+    ContentBlock *takeBlock()
+    {
+        return m_contentBlock.release();
+    }
+};
+}
+
 ContentModel::ContentModel(QObject *parent)
     : QAbstractListModel(parent)
 {
@@ -32,13 +58,34 @@ void ContentModel::setText(const QString &text)
     if (m_text != text) {
         m_text = text;
         beginResetModel();
-        m_blocks = HtmlSplitter::cleanHtml(text, this);
+        QCoreApplication::removePostedEvents(this, kAddBlockEventType);
+        qDeleteAll(m_blocks);
+        m_blocks.clear();
         endResetModel();
         emit textChanged();
+
+        const QVector<ContentBlock *> blocks = HtmlSplitter::cleanHtml(text, nullptr);
+        for (ContentBlock *block : blocks) {
+            QCoreApplication::postEvent(this, new AddBlockEvent(block), Qt::LowEventPriority);
+        }
     }
 }
 
 QHash<int, QByteArray> ContentModel::roleNames() const
 {
     return {{Qt::UserRole, "block"}};
+}
+
+void ContentModel::customEvent(QEvent *event)
+{
+    if (auto *addBlockEvent = dynamic_cast<AddBlockEvent *>(event)) {
+        int idx = m_blocks.count();
+        beginInsertRows(QModelIndex(), idx, idx);
+        ContentBlock *block = addBlockEvent->takeBlock();
+        block->setParent(this);
+        m_blocks.append(block);
+        endInsertRows();
+    } else {
+        QAbstractListModel::customEvent(event);
+    }
 }
