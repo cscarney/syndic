72d4f91f5237ec77d5594d17a5b95682b300084c
Connor Carney <hello@connorcarney.com>
2022-09-03 20:55:54 -0500

    Build content model in background thread
    This amends fab1b65f7c44a48099b3890be9a4b9e92922c016 to do parsing
    and content blocking in a background thread.

--- src/contentmodel.cpp
+++ src/contentmodel.cpp
@@ -7,7 +7,14 @@
 
 #include <QCoreApplication>
 #include <QEvent>
+#include <QMutex>
+#include <QPointer>
+#include <QRunnable>
+#include <QThreadPool>
 #include <memory>
+#include <utility>
+
+#include <QDebug>
 
 namespace
 {
@@ -24,17 +31,57 @@ public:
     {
     }
 
-    ContentBlock *takeBlock()
+    ContentBlock *takeBlock(QObject *newParent)
     {
+        m_contentBlock->setParent(newParent);
         return m_contentBlock.release();
     }
 };
 }
 
+class ContentModel::ParseJob : public QRunnable
+{
+    QString m_text;
+    QMutex m_runLock;
+    std::atomic_bool m_cancelled{false};
+    QObject *m_target;
+
+public:
+    Q_DISABLE_COPY_MOVE(ParseJob)
+    ParseJob(QObject *target, QString text)
+        : m_text(std::move(text))
+        , m_target(target)
+    {
+        setAutoDelete(false);
+    }
+
+    ~ParseJob() override
+    {
+        // block destruction while job is running
+        m_cancelled = true;
+        QMutexLocker lock(&m_runLock);
+    }
+
+    void run() override
+    {
+        QMutexLocker lock(&m_runLock);
+        const QVector<ContentBlock *> blocks = HtmlSplitter::cleanHtml(m_text, nullptr);
+        for (ContentBlock *block : blocks) {
+            if (m_cancelled) {
+                return;
+            }
+            block->moveToThread(m_target->thread());
+            QCoreApplication::postEvent(m_target, new AddBlockEvent(block), Qt::LowEventPriority);
+        }
+    }
+};
+
 ContentModel::ContentModel(QObject *parent)
     : QAbstractListModel(parent)
 {
 }
+
+ContentModel::~ContentModel() = default;
 
 int ContentModel::rowCount(const QModelIndex &parent) const
 {
@@ -57,17 +104,18 @@ void ContentModel::setText(const QString &text)
 {
     if (m_text != text) {
         m_text = text;
+
+        if (!m_text.isEmpty()) {
+            m_job = std::make_unique<ParseJob>(this, text);
+            QCoreApplication::removePostedEvents(this, kAddBlockEventType);
+            QThreadPool::globalInstance()->start(m_job.get());
+        }
+
         beginResetModel();
-        QCoreApplication::removePostedEvents(this, kAddBlockEventType);
         qDeleteAll(m_blocks);
         m_blocks.clear();
         endResetModel();
         emit textChanged();
-
-        const QVector<ContentBlock *> blocks = HtmlSplitter::cleanHtml(text, nullptr);
-        for (ContentBlock *block : blocks) {
-            QCoreApplication::postEvent(this, new AddBlockEvent(block), Qt::LowEventPriority);
-        }
     }
 }
 
@@ -79,10 +127,10 @@ QHash<int, QByteArray> ContentModel::roleNames() const
 void ContentModel::customEvent(QEvent *event)
 {
     if (auto *addBlockEvent = dynamic_cast<AddBlockEvent *>(event)) {
+        assert(QThread::currentThread() == this->thread());
         int idx = m_blocks.count();
         beginInsertRows(QModelIndex(), idx, idx);
-        ContentBlock *block = addBlockEvent->takeBlock();
-        block->setParent(this);
+        ContentBlock *block = addBlockEvent->takeBlock(this);
         m_blocks.append(block);
         endInsertRows();
     } else {
