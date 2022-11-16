10a8d027d5ff513271e7fdc4a01ed8a2573c96c1
Connor Carney <hello@connorcarney.com>
2022-11-16 15:22:12 -0600

    Refactor Readability class
    Readability now returns an object representing a pending result instead
    of emitting result signals directly from the readability class. This
    simplifies calling code: callers no longer have to filter out results
    from multiple requests, or manually disconnect from the result signals
    when the operation completes.

--- feedcore/readability/qreadablereadability.cpp
+++ feedcore/readability/qreadablereadability.cpp
@@ -5,6 +5,7 @@
 
 #include "qreadablereadability.h"
 #include "networkaccessmanager.h"
+#include "readabilityresult.h"
 #include <QDebug>
 #include <QNetworkReply>
 #include <QString>
@@ -13,25 +14,49 @@
 
 using namespace FeedCore;
 
+class QReadableReadability::Result : public ReadabilityResult
+{
+    QNetworkReply *m_reply;
+    QReadableReadability *m_readability;
+
+public:
+    Result(QReadableReadability *parent, QNetworkReply *reply)
+        : ReadabilityResult(parent)
+        , m_reply{reply}
+        , m_readability{parent}
+    {
+        reply->setParent(this);
+        QObject::connect(reply, &QNetworkReply::finished, this, &Result::onNetworkReplyFinished);
+    }
+
+    void onNetworkReplyFinished()
+    {
+        deleteLater();
+        if (m_reply->error() != QNetworkReply::NoError) {
+            emit error();
+            return;
+        }
+        QByteArray data = m_reply->readAll();
+        QString rawHtml(data);
+        QString readableHtml = m_readability->parse(rawHtml, m_reply->url());
+        emit finished(readableHtml);
+    }
+};
+
 QReadableReadability::QReadableReadability()
     : m_readable{new QReadable::Readable(this)}
 {
 }
 
-void QReadableReadability::fetch(const QString &url)
+ReadabilityResult *QReadableReadability::fetch(const QUrl &url)
 {
     auto *nam = NetworkAccessManager::instance();
     QNetworkRequest req(url);
     auto *reply = nam->get(req);
-    emit startedFetching(url);
-    QObject::connect(reply, &QNetworkReply::finished, this, [this, url, reply] {
-        if (reply->error() == QNetworkReply::NoError) {
-            QByteArray data = reply->readAll();
-            QString rawHtml(data);
-            QString readableHtml = m_readable->parse(rawHtml, reply->url());
-            emit finishedFetching(url, readableHtml);
-        } else {
-            emit errorFetching(url);
-        }
-    });
+    return new Result(this, reply);
+}
+
+QString QReadableReadability::parse(const QString &rawHtml, const QUrl &url)
+{
+    return m_readable->parse(rawHtml, url);
 }
