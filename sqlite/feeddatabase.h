a32345e8e91ed15205c48ec66b1db460169fa570
Connor Carney <hello@connorcarney.com>
2023-12-31 15:22:16 -0600

    Increase number of highlights from 10 to 20

--- sqlite/feeddatabase.h
+++ sqlite/feeddatabase.h
@@ -24,7 +24,7 @@ public:
     ItemQuery selectUnreadItems();
     ItemQuery selectStarredItems();
     ItemQuery selectItemsBySearch(const QString &search);
-    static constexpr const int kDefaultNumberOfRecommendedItems = 10;
+    static constexpr const int kDefaultNumberOfRecommendedItems = 20;
     ItemQuery selectItemsByRecommended(int limit = kDefaultNumberOfRecommendedItems);
     ItemQuery selectItemsByFeed(qint64 feedId);
     ItemQuery selectUnreadItemsByFeed(qint64 feedId);
