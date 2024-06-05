5f7a408f8345f1d22f602763bfdede43fa090c3d
Connor Carney <hello@connorcarney.com>
2024-06-05 11:15:22 -0500

    Set fractional scale rounding policy
    fixes #235

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
     // HACK readability breaks the arm64 jit, so disable it
     qputenv("QV4_FORCE_INTERPRETER", "1");
 #endif
+    // https://github.com/cscarney/syndic/issues/235
+    // https://bugs.kde.org/show_bug.cgi?id=479891
+    // Contrary to the documentation, this does not seem to change the scale factor,
+    // but it does fix the graphical glitches that otherwise occur at non-integral
+    // scale factors.
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::RoundPreferFloor);
     Application app(argc, argv);
 
     if (!qEnvironmentVariableIsSet("QT_QUICK_CONTROLS_STYLE")) {
